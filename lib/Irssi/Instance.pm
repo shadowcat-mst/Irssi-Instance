679e2b661c72fdc7a644e115ba684556b842ea83
Matt S Trout <mst@shadowcat.co.uk>
2020-04-07 15:10:29 +0000

    skeleton version+docs from distar-init

--- lib/Irssi/Instance.pm
+++ lib/Irssi/Instance.pm
@@ -1,5 +1,9 @@
 package Irssi::Instance;
 
+our $VERSION = '0.000001'; # 0.0.1
+
+$VERSION = eval $VERSION;
+
 use Irssi::Instance::SocketClient;
 use Import::Into;
 use Mojo::Base qw(Irssi::Instance::Base -signatures -async_await);
@@ -18,3 +22,29 @@ async sub start ($self) {
 }
 
 1;
+
+=head1 NAME
+
+Irssi::Instance - Description goes here
+
+=head1 SYNOPSIS
+
+=head1 DESCRIPTION
+
+=head1 AUTHOR
+
+ mst - Matt S. Trout (cpan:MSTROUT) <mst@shadowcat.co.uk>
+
+=head1 CONTRIBUTORS
+
+None yet - maybe this software is perfect! (ahahahahahahahahaha)
+
+=head1 COPYRIGHT
+
+Copyright (c) 2020 the Irssi::Instance L</AUTHOR> and L</CONTRIBUTORS>
+as listed above.
+
+=head1 LICENSE
+
+This library is free software and may be distributed under the same terms
+as perl itself.
