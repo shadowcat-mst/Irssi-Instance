5a206e9ed4ccacbbbd53bc101fc3b40ef9f42d9d
Matt S Trout <mst@shadowcat.co.uk>
2020-04-12 00:02:47 +0000

    $server->("tag") is much nicer than ->get_attr

--- lib/Irssi/Instance/RemoteObject.pm
+++ lib/Irssi/Instance/RemoteObject.pm
@@ -2,7 +2,15 @@ package Irssi::Instance::RemoteObject;
 
 use Mojo::Base qw(Irssi::Instance::Base -signatures);
 
+use overload
+  '&{}' => sub { shift->get_attr_closure },
+  fallback => 1;
+
 has 'attrs';
+
+sub get_attr_closure ($self) {
+  sub ($key) { $self->get_attr($key) }
+}
 
 sub get_attr ($self, $key) { $self->attrs->{$key} }
 
